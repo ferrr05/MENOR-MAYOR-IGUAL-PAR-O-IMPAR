fba96ab941ea20081beb224a1b570bacfaf5baf7
ferrr05 <123724312+ferrr05@users.noreply.github.com>
2023-04-18 12:09:20 +0200

    final

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 let num1 = parseFloat(prompt("Primer Número:"))
 let num2 = parseFloat(prompt("Segundo Número"))
-if (num1>num2) {alert("El número" + num1 + "es mayor que el número" + num2)}
+if (num1 > num2) { 
+    alert("El número" + num1 + "es mayor que el número" + num2) 
+}
+else if (num1<num2){
+    alert("El número " + num2 + "es mayor que el número " + num1)
+}
+else if(num1==num2){
+    alert("El número "+ num1 + "y el número " + num2 + "son iguales ")
+}
+if (num2 % 2 == 0){
+    alert("El número " + num2 + "es par")
+}
+else{
+    alert("El número " + num2 + "es impar")
+}
